5970af82d27227f3719c8ac89c7732f2b14839fc
tianyaruqian <418059544@qq.com>
2019-11-01 01:09:38 +0800

    上传文件

---
+++ shop-portal-boot/src/main/java/com/fh/shop/portal/boot/ShopPortalBootApplication.java
@@ -0,0 +1,13 @@
+package com.fh.shop.portal.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPortalBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPortalBootApplication.class, args);
+    }
+
+}
