8b1c7b6d8a718a6dc6db13fd353023dd0ff94d9e
tianyaruqian <tianyaruqian@163.com>
2021-03-07 19:29:14 +0800

    从frame类继承,重写main方法，出现黑方块

---
+++ tank/src/main/java/com/hou/tank/TankFrame.java
@@ -0,0 +1,30 @@
+package com.hou.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @author 天涯如浅
+ */
+public class TankFrame extends Frame {
+
+    public TankFrame(){
+        setSize(800,600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+               System.exit(0);
+            }
+        });
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(200,200,50,50);
+    }
+}
